37f3f5538590e29f581816b5c26aedb342309c38
RillaryDev <rillarydev@gmail.com>
2023-06-28 14:45:42 -0300

    Task 3

---
+++ src/test/java/com/projeto/minasnarea/MinasnaareaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.minasnarea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinasnaareaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
